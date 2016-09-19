9f5c341d74c26fe044ffe3fba1fc8335e71ba2e8
stasundr <stasundr@gmail.com>
2016-09-20 02:00:49 +0600

    Binding map to redux store (fast fix 2)

--- src/components/map/duck.js
+++ src/components/map/duck.js
@@ -24,13 +24,13 @@ const reducer = (state = initialState, action) => {
             return Object.assign({}, state, {
                 mapCenterLat: action.payload.lat,
                 mapCenterLng: action.payload.lng
-                // TODO: ADD OTHER PROPS
             });
 
         case _c.MAP_ON_CHANGE:
             return Object.assign({}, state, {
-                mapZoom: action.payload.zoom
-                // TODO: ADD OTHER PROPS
+                mapZoom: action.payload.zoom,
+                mapCenterLat: action.payload.center.lat,
+                mapCenterLng: action.payload.center.lng
             });
 
         default:
