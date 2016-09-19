121f35055af954f43f3574baf3a161280c6ac23e
stasundr <stasundr@gmail.com>
2016-09-20 01:55:55 +0600

    Binding map to redux store (fast fix)

--- src/store/root_reducer.js
+++ src/store/root_reducer.js
@@ -3,9 +3,13 @@
 //import { combineReducers } from 'redux';
 import initialState from './initial_state';
 import tabsReducer from '../components/tabs/duck';
+import mapReducer from '../components/map/duck';
 
 const rootReducer = (state = initialState, action) => {
-    return tabsReducer(state, action);
+    return mapReducer(
+        tabsReducer(state, action),
+        action
+    );
 };
 
 export default rootReducer;
